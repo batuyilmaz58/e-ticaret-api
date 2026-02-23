699111a5208156cca49080a807e42401c063486a
Batuhan <batuhanyilmaz0011@gmail.com>
2026-02-23 20:37:44 +0300

    third commit

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env()
+env = os.environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
